539bcc85e0161e4a8338b24164aaf23d5fec80cb
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-03-03 07:04:34 +0000

    Complete the nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -17,102 +17,106 @@ const Nav = () => {
   };
 
   return (
-    <nav
-      className={`flex flex-col z-10 bg px-8 py-7 ${
-        mobileMenu ? "h-screen " : "h-fit"
-      } fixed top-0 w-full shadow-xl md:flex-row md:justify-between md:px-20 md:h-fit`}
-    >
-      <div className="flex items-center">
-        <a href="#" className="">
-          <img src={logo} alt="Urubyte" className="w-auto h-8 md:h-10" />
-        </a>
-      </div>
-
-      <nav>
-        <div className="md:hidden mobile-btns">
-          <button
-            type="button"
-            className={classNames(!mobileMenu ? "block" : "hidden")}
-            onClick={() => setMobileMenu(true)}
-          >
-            <MenuIcon className="h-8 mr-2" />
-          </button>
-          <button
-            type="button"
-            className={classNames(mobileMenu ? "block" : "hidden")}
-            onClick={() => setMobileMenu(false)}
-          >
-            <XIcon className="h-8 mr-2" />
-          </button>
-        </div>
-        <div
-          className={classNames(mobileMenu ? "block" : "hidden", "md:block")}
-        >
-          <ul className="flex flex-col gap-8 items-center mt-4 md:flex-row md:mt-0">
-            <li>
-              <a
-                href="#"
-                onClick={() => setNav("#")}
-                className={activeNav === "#" ? "active" : ""}
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="#about"
-                onClick={() => setNav("#about")}
-                className={activeNav === "#about" ? "active" : ""}
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href="#skills"
-                onClick={() => setNav("#skills")}
-                className={activeNav === "#skills" ? "active" : ""}
-              >
-                Skills
-              </a>
-            </li>
-            <li>
-              <a
-                href="#project"
-                onClick={() => setNav("#project")}
-                className={activeNav === "#project" ? "active" : ""}
-              >
-                Project
-              </a>
-            </li>
-            <li>
-              <a
-                href="#contact"
-                onClick={() => setNav("#contact")}
-                className={activeNav === "#contact" ? "active" : ""}
-              >
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-      <a
-        href="#login"
-        className={`self-center ${
-          mobileMenu ? "block" : "hidden"
-        } md:block btn:hover  activeNav === "#login" ? "active" : ""`}
+    <div>
+      <nav
+        className={`flex flex-col z-10 bg px-8 py-7 ${
+          mobileMenu ? "h-screen " : "h-fit"
+          // } fixed top-0 w-full border-b-4 border-black md:flex-row md:justify-between md:px-20 md:h-fit`}
+        } fixed top-0 w-full border-b-4 border-black md:flex-row md:justify-between md:px-20 md:h-fit`}
       >
-        <button
-          onClick={() => setMobileMenu(false)}
-          type="button"
-          className={`border-2 mt-4 md:mt-0 rounded px-4 py-1 w-32 hover:text-skyColor`}
+        <div className="flex ">
+          <a href="#" className="">
+            <img src={logo} alt="Urubyte" className="w-auto h-8 md:h-10" />
+          </a>
+        </div>
+
+        <nav>
+          <div className="md:hidden mobile-btns">
+            <button
+              type="button"
+              className={classNames(!mobileMenu ? "block" : "hidden")}
+              onClick={() => setMobileMenu(true)}
+            >
+              <MenuIcon className="h-8 mr-2" />
+            </button>
+            <button
+              type="button"
+              className={classNames(mobileMenu ? "block" : "hidden")}
+              onClick={() => setMobileMenu(false)}
+            >
+              <XIcon className="h-8 mr-2" />
+            </button>
+          </div>
+          <div
+            className={classNames(mobileMenu ? "block" : "hidden", "md:block")}
+          >
+            <ul className="flex flex-col gap-8 items-center mt-4 md:flex-row md:mt-2">
+              <li>
+                <a
+                  href="#"
+                  onClick={() => setNav("#")}
+                  className={activeNav === "#" ? "active" : ""}
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#about"
+                  onClick={() => setNav("#about")}
+                  className={activeNav === "#about" ? "active" : ""}
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#skills"
+                  onClick={() => setNav("#skills")}
+                  className={activeNav === "#skills" ? "active" : ""}
+                >
+                  Skills
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#project"
+                  onClick={() => setNav("#project")}
+                  className={activeNav === "#project" ? "active" : ""}
+                >
+                  Project
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#contact"
+                  onClick={() => setNav("#contact")}
+                  className={activeNav === "#contact" ? "active" : ""}
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <a
+          href="#login"
+          className={`self-center ${
+            mobileMenu ? "block" : "hidden"
+          } md:block btn:hover  activeNav === "#login" ? "active" : ""`}
         >
-          Get Started
-        </button>
-      </a>
-    </nav>
+          <button
+            onClick={() => setMobileMenu(false)}
+            type="button"
+            className={`border-2 mt-4 md:mt-0 rounded px-4 py-1 w-32 hover:text-skyColor btn`}
+          >
+            Get Started
+          </button>
+        </a>
+      </nav>
+      <p className="line">helloooo</p>
+    </div>
   );
 };
 
