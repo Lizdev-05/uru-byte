0d29ab284c2cd28d5b7c9a112f345e87edd37f4e
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-03-07 19:30:09 +0000

    Complete work on the contact page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./pages/about/About";
 import Feature from "./pages/features/Feature";
 import Team from "./pages/team/Team";
 import Faq from "./pages/faq/Faq";
+import Contact from "./pages/contact/Contact";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Feature />
       <Team />
       <Faq />
+      <Contact />
     </div>
   );
 }
