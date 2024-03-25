eff90c88ebfa88309cae1adfd65284e30be37896
Elizabeth Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2024-03-25 09:32:36 +0000

    Remove streams as suggested by Andrews

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,7 +12,7 @@ const About = () => {
             Unlock critical insights on companies, topics, and industries across
             an extensive universe of content—including your own.
           </p> */}
-          <h1>Centralize and Sync Your Data Streams for Unified Insights</h1>
+          <h1>Centralize and Sync Your Data for Unified Insights</h1>
           <p>
             Transform unstructured data from diverse sources into customizable,
             insightful outputs.
